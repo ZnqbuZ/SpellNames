015e34d5d898002950143a27005c3d01fe981913
Luna Yao <github@me.2n9.co>
2025-03-22 16:32:57 +0100

    Up date example

--- example/example.ts
+++ example/example.ts
@@ -1,9 +1,14 @@
-import {splitName} from 'spellnames';
-// import {splitName} from '../dist/index.mjs';
+// import {CjkName, Language, splitName} from 'spellnames';
+import * as sn from '../dist/index.mjs';
 
-for (let name of ['陈李四', '欧阳王五', '刘六六六', '司马仲达', '阿里巴巴']) {
-    console.log(splitName(name, 'Chinese').familyName.transliteration);
+for (let name of ['陈李四', '欧阳王五', '刘六六六', '司马仲达', '阿里巴巴', 'John Smith', '肖发民']) {
+    console.log(sn.splitName(name, 'Chinese').isName);
+    const cjkName = sn.CjkName.fromString(name, sn.Language.Chinese, (_) => _, true, false, undefined);
+    console.log(cjkName)
 }
 for (let name of ['張三', '陳李四', '歐陽王五', '劉六六六', '諸葛亮', '東方不敗', '阿裡巴巴']) {
-    console.log(splitName(name, 'Chinese', (_) => "__PINYIN__").getFullNameTransliteration());
+    console.log(sn.splitName(name, 'Chinese', (_) => "__PINYIN__").getFullNameTransliteration());
 }
+
+console.log(sn.splitName("Flav", 'Chinese'))
+
